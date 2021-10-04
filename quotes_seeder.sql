a8744a3666ef312074c3b4d004fc0ba34bfdf728
Ian Arellano <ian.g.arellano01@gmail.com>
2021-10-04 09:46:27 -0500

    add statement lecture and a quote seeder practice

---
+++ quotes_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+
+INSERT INTO quotes(author_first_name, author_last_name, content) VALUES
+('First1', 'Last1', 'Quote1'),
+('First2', 'Last2', 'Quote2'),
+('First3', 'Last3', 'Quote3'),
+('First4', 'Last4', 'Quote4'),
+('First5', 'Last5', 'Quote5');
