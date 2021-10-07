6aad81785142eb1a7d859cb7aed2b1633ac18437
Ian Arellano <ian.g.arellano01@gmail.com>
2021-10-07 17:00:25 -0500

    solved bonus for join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -31,10 +31,14 @@ WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 ORDER BY dept_name;
 
 
-# Unable to solve bonus. Can't figure out how to port department manager without overriding employees.
-# SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee,
-#        d.dept_name AS Department
-# FROM employees e
-#     JOIN dept_emp de on e.emp_no = de.emp_no
-#     JOIN departments d on d.dept_no = de.dept_no
-# WHERE de.to_date = '9999-01-01';
+# Solved bonus thanks to solution provided by Chris
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee,
+       d.dept_name AS Department,
+        CONCAT(e2.first_name, ' ', e2.last_name) AS Manager
+FROM employees e
+    JOIN dept_emp de on e.emp_no = de.emp_no
+    JOIN departments d on d.dept_no = de.dept_no
+    JOIN dept_manager dm on d.dept_no = dm.dept_no
+    JOIN employees e2 ON e2.emp_no = dm.emp_no
+WHERE de.to_date = '9999-01-01'AND dm.to_date = '9999-01-01'
+ORDER BY d.dept_name, e.emp_no;
